c79640db3fa9719a0a8577401de472288ea754a9
Robert <rreina05@gmail.com>
2026-02-16 12:29:54 -0500

    actaulzando vriable apiUrl para entornos de develop y testing

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: '',
+    apiUrl: 'https://ts-admin-api-dev.prodevsolution.dev/api',
     // apiUrl: 'http://localhost:3000/api',
     // TS Dev
     firebaseConfig: {
--- src/environments/environment.testing.ts
+++ src/environments/environment.testing.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: '',  
+    apiUrl: 'https://ts-admin-api-testing.prodevsolution.dev/api',  
     // TS Testing
     firebaseConfig: {
         apiKey: "AIzaSyANPJNtWUEeoZpPBBssBfuczJxso732KFA",
