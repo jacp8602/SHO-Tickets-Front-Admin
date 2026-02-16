f1ffa336191718ec5174fdcb9ad75a4698735f46
Robert <rreina05@gmail.com>
2026-02-16 11:30:53 -0500

    ficheros de entornos

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: false,
+    apiUrl: '',
+    // apiUrl: 'http://localhost:3000/api',
+    // TS Dev
+    firebaseConfig: {
+        apiKey: "AIzaSyANPJNtWUEeoZpPBBssBfuczJxso732KFA",
+        authDomain: "sho-management-dev.firebaseapp.com",
+        projectId: "sho-management-dev",
+        storageBucket: "ts-admin-dev",
+        messagingSenderId: "1013987727818",
+        appId: "1:1013987727818:web:c97a5f5db85b0c41851733",
+        measurementId: "G-PX9MC8M6SZ"
+    }
+};
---
+++ src/environments/environment.testing.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    apiUrl: '',  
+    // TS Testing
+    firebaseConfig: {
+        apiKey: "AIzaSyANPJNtWUEeoZpPBBssBfuczJxso732KFA",
+        authDomain: "sho-management-dev.firebaseapp.com",
+        projectId: "sho-management-dev",
+        storageBucket: "ts-admin-testing",
+        messagingSenderId: "1013987727818",
+        appId: "1:1013987727818:web:21217818550cb2a2851733",
+        measurementId: "G-8BFKZNCBTQ"
+    }
+};
