c82d3de096fe6530b266a65481bfa16d8e5b7880
Robert <rreina05@gmail.com>
2026-02-19 10:07:13 -0500

    route fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -76,7 +76,6 @@ export const appRoutes: Route[] = [
         },
         children: [
             {path: 'example', loadChildren: () => import('app/modules/admin/example/example.routes')},
-            {path: 'example01', loadChildren: () => import('app/modules/admin/example/example.routes')},
         ]
     }
 ];
