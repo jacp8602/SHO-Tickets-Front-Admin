06a45ea602f25981d5561c0dc4fef996aeaaf0ed
Javier Cobas <jacp8602@gmail.com>
2026-03-11 00:20:14 -0400

    ADD -> From UI Figma to Financial Reports / Overview Total Sales

---
+++ src/app/modules/admin/overview-total-sales/overview-total-sales.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { OverviewTotalSalesComponent } from './overview-total-sales.component';
+
+export default [
+    {
+        path: '',
+        component: OverviewTotalSalesComponent,
+    },
+] as Routes;
